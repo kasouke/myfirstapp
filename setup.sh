93ef4e52aaf083632064c23529feeecb0bde99cc
kasouke <87646547+kasouke@users.noreply.github.com>
2021-07-19 20:52:58 +0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
+{\colortbl;\red255\green255\blue255;\red189\green198\blue208;\red198\green136\blue27;\red0\green0\blue0;
+\red12\green14\blue18;\red150\green204\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c78824\c81961\c85098;\cssrgb\c82353\c60000\c13333\c14902;\cssrgb\c0\c0\c0;
+\cssrgb\c5098\c6667\c9020;\cssrgb\c64706\c83922\c100000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 mkdir -p \cf4 \cb1 \strokec4 ~\cf2 \cb3 \strokec2 /.streamlit/\
+\
+\pard\pardeftab720\partightenfactor0
+\cf4 \cb5 \strokec4 echo\cf2 \strokec2  \cf4 \strokec4 "\\\
+\pard\pardeftab720\partightenfactor0
+\cf6 \strokec6 [server]\\n\\\
+headless = true\\n\\\
+port = \cf4 \strokec4 $PORT\cf6 \strokec6 \\n\\\
+enableCORS = false\\n\\\
+\pard\pardeftab720\partightenfactor0
+\cf4 \strokec4 "\cf2 \strokec2  \cf4 \strokec4 >\cf2 \strokec2  \cf4 \strokec4 ~\cf2 \strokec2 /.streamlit/config.toml}
